5e35f65d388e46dad9ae054aa561ae96f4025c6f
Adriana Jacobo <adriana.jacobo@catolica.edu.sv>
2024-04-15 17:28:23 -0600

    Config de Controlador y Vista

---
+++ DulceSabor-Modulo2/Controllers/MesasController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DulceSabor_Modulo2.Controllers
+{
+    public class MesasController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
