6bbf022d0fcd631be90ef9aa8e05188ad75655da
Adriana Jacobo <adriana.jacobo@catolica.edu.sv>
2024-04-16 11:27:10 -0600

    Maquetación de tabs de mesas disponibles y no disponibles, y funcionamiento de estas

--- DulceSabor-Modulo2/Controllers/MesasController.cs
+++ DulceSabor-Modulo2/Controllers/MesasController.cs
@@ -1,12 +1,32 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DulceSabor_Modulo2.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace DulceSabor_Modulo2.Controllers
 {
     public class MesasController : Controller
     {
+        private readonly RestauranteDbContext _restauranteContext;
+
+        public MesasController(RestauranteDbContext restauranteContext)
+        {
+            _restauranteContext = restauranteContext;
+        }
+
         public IActionResult Index()
         {
+
+            var listaDeMesas = (from m in _restauranteContext.Mesas
+                                select new
+                                {
+                                    nummesa = m.NumeroMesa
+                                }).ToList();
+
+            ViewData["listadoMesas"] = listaDeMesas;
+
             return View();
+
+
         }
         public IActionResult Detalle()
         {
